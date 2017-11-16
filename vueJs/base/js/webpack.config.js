276aadb24d72da40b679a0f1aa6f21c3c74ce9fb
sunny327 <dzliwen@163.com>
2017-11-17 00:07:49 +0800

    vue.js

---
+++ vueJs/base/js/webpack.config.js
@@ -0,0 +1,16 @@
+/**
+ * Created by liwenzhi on 2017/11/5.
+ */
+
+var webpack = require('webpack')
+module.exports = {
+    // ...
+    plugins: [
+        // ...
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        })
+    ]
+}
