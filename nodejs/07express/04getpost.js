204a067a4458baf291f6a079c9192afad1b422d5
sunny327 <dzliwen@163.com>
2019-08-08 23:12:40 +0800

    aa

--- nodejs/07express/04getpost.js
+++ nodejs/07express/04getpost.js
@@ -13,11 +13,7 @@ app.use(bodyParser.json())
 
 app.get('/', function (req, res, next) {
   console.log('getreq:--', req.query)
-<<<<<<< HEAD
   res.render('form')
-=======
-  res.render('./form.ejs')
->>>>>>> 24d5c041e98742bf3a762479e9e9775ef8a94be0
   next()
 })
 
@@ -26,16 +22,8 @@ app.post('/', function (req, res) {
   /* res.setHeader('Content-Type', 'text/plain')
   res.write('you posted:\n')
   res.end(JSON.stringify(req.body, null, 2)) */
-<<<<<<< HEAD
   res.render('form.ejs')
   console.log(req.body)
 })
 
 app.listen(4000)
-=======
-  res.render('./form.ejs')
-  console.log(req.body)
-})
-
-app.listen(2000)
->>>>>>> 24d5c041e98742bf3a762479e9e9775ef8a94be0
